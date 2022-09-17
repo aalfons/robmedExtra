87719d80a40d1b120895bf9f93ee19c7ec5f859a
vdrenth <v.l.drenth@student.tudelft.nl>
2022-09-17 09:44:06 +0200

    Moved previous robmed page to a tabsetpanel

--- inst/shiny-examples/robmedshinyApp/ui.R
+++ inst/shiny-examples/robmedshinyApp/ui.R
@@ -18,41 +18,49 @@ shinyUI(fluidPage(theme = shinytheme("cosmo"),
     # Application title
     titlePanel( h2("ROBMED", align = 'center')),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(actionButton('run', "Run"),
-                     h2("Settings"),
-                     
+    tabsetPanel(
+      tabPanel("main",
+               sidebarLayout(
+                 sidebarPanel(actionButton('run', "Run"),
+                              h2("Settings"),
 
-        fileInput("file", "Choose CSV File",accept = c("text/csv",
-                                                         "text/comma-separated-values,text/plain",
-                                                          ".csv")
-                  ),
-        
-        uiOutput('selectUI'),
-        uiOutput('selectResponse'),
-        uiOutput('selectExplanatory'),
-        uiOutput('selectControls'),
-        uiOutput('selectMediator'),
-        
-        selectInput("Modeltype", "Type of mediator",
-                    choices = c('parallel', 'serial')),
-        
-        sliderInput("Confidence", "Confidence level test",
-                    min = 0, max = 1, value = 0.95),
-      
-        numericInput('seed', label = 'Random seed', value = 0)
-        ),
-        
 
-        # Show a plot of the generated distribution
-        mainPanel(hr(),
-          h3('Diagnostic plot'),             
-            plotOutput("plot_weights"),
-            hr(),
-            h3('Model and Test summary'),
-            verbatimTextOutput('summary'),
-            verbatimTextOutput('formula')
-            )
+                              fileInput("file", "Choose CSV File",accept = c("text/csv",
+                                                                             "text/comma-separated-values,text/plain",
+                                                                             ".csv")
+                              ),
+
+                              uiOutput('selectUI'),
+                              uiOutput('selectResponse'),
+                              uiOutput('selectExplanatory'),
+                              uiOutput('selectControls'),
+                              uiOutput('selectMediator'),
+
+                              selectInput("Modeltype", "Type of mediator",
+                                          choices = c('parallel', 'serial')),
+
+                              sliderInput("Confidence", "Confidence level test",
+                                          min = 0, max = 1, value = 0.95),
+
+                              numericInput('seed', label = 'Random seed', value = 0)
+                 ),
+
+
+                 # Show a plot of the generated distribution
+                 mainPanel(hr(),
+                           h3('Diagnostic plot'),
+                           plotOutput("plot_weights"),
+                           hr(),
+                           h3('Model and Test summary'),
+                           verbatimTextOutput('summary'),
+                           verbatimTextOutput('formula')
+                 )
+               )
+
+               ),
+    tabPanel('View Data',
+             h1("Data")
+             )
+
     )
 ))
