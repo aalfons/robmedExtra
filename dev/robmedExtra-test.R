275b6ca1ceaa2c8f974dd00d51414dc47e8f7d8a
Andreas Alfons <alfons@ese.eur.nl>
2023-02-23 10:14:44 +0100

    updated files for testing

--- dev/robmedExtra-test.R
+++ dev/robmedExtra-test.R
@@ -1,4 +1,4 @@
-library("robmed")
+library("robmedExtra")
 data("BSG2014")
 
 ## seed to be used for the random number generator
@@ -7,16 +7,11 @@ seed <- 20211117
 ## simple mediation
 # set seed of the random number generator
 set.seed(seed)
-# The results in Alfons et al. (2021) were obtained with an
-# older version of the random number generator.  To reproduce
-# those results, uncomment the two lines below.
-# RNGversion("3.5.3")
-# set.seed(20150601)
 # perform mediation analysis
-boot_simple <- test_mediation(TeamCommitment ~
-                                m(TaskConflict) +
-                                ValueDiversity,
-                              data = BSG2014)
+boot_simple <- test_mediation(BSG2014,
+                              x = "ValueDiversity",
+                              y = "TeamCommitment",
+                              m = "TaskConflict")
 summary(boot_simple)
 to_latex(boot_simple)
 
@@ -25,12 +20,11 @@ to_latex(boot_simple)
 # set seed of the random number generator
 set.seed(seed)
 # perform mediation analysis
-boot_serial <- test_mediation(TeamScore ~
-                                serial_m(TaskConflict,
-                                         TeamCommitment) +
-                                ValueDiversity +
-                                covariates(AgeDiversity),
-                              data = BSG2014)
+boot_serial <- test_mediation(BSG2014,
+                              x = "ValueDiversity",
+                              y = "TeamScore",
+                              m = c("TaskConflict", "TeamCommitment"),
+                              model = "serial")
 summary(boot_serial)
 to_latex(boot_serial)
 
@@ -39,55 +33,74 @@ to_latex(boot_serial)
 # set seed of the random number generator
 set.seed(seed)
 # perform mediation analysis
-boot_parallel <- test_mediation(TeamPerformance ~
-                                  parallel_m(ProceduralJustice,
-                                             InteractionalJustice) +
-                                  SharedLeadership +
-                                  covariates(AgeDiversity, GenderDiversity),
-                                data = BSG2014)
+boot_parallel <- test_mediation(BSG2014,
+                                x = "SharedLeadership",
+                                y = "TeamPerformance",
+                                m = c("ProceduralJustice",
+                                      "InteractionalJustice"),
+                                covariates = c("AgeDiversity",
+                                               "GenderDiversity"))
 summary(boot_parallel)
 to_latex(boot_parallel)
 
 
-## parallel multiple mediators and control variables
+## multiple independent variables
 # set seed of the random number generator
 set.seed(seed)
 # perform mediation analysis
-boot_multiple <- test_mediation(TeamPerformance ~
-                                  m(ProceduralJustice) +
-                                  SharedLeadership +
-                                  AgeDiversity +
-                                  GenderDiversity,
-                                data = BSG2014)
+boot_multiple <- test_mediation(BSG2014,
+                                x = c("SharedLeadership",
+                                      "AgeDiversity",
+                                      "GenderDiversity"),
+                                y = "TeamPerformance",
+                                m = "ProceduralJustice")
 summary(boot_multiple)
 to_latex(boot_multiple)
 
 
-# bootstrap test with covariance fit
-cov_boot <- test_mediation(TeamCommitment ~
-                             m(TaskConflict) +
-                             ValueDiversity,
-                           data = BSG2014,
+## parallel multiple mediators and multiple independent variables
+# set seed of the random number generator
+set.seed(seed)
+# perform mediation analysis
+boot_mult_par <- test_mediation(BSG2014,
+                                x = c("SharedLeadership",
+                                      "AgeDiversity",
+                                      "GenderDiversity"),
+                                y = "TeamPerformance",
+                                m = c("ProceduralJustice",
+                                      "InteractionalJustice"))
+summary(boot_mult_par)
+to_latex(boot_mult_par)
+
+
+## bootstrap test with covariance fit
+# set seed of the random number generator
+set.seed(seed)
+# perform mediation analysis
+cov_boot <- test_mediation(BSG2014,
+                           x = "ValueDiversity",
+                           y = "TeamCommitment",
+                           m = "TaskConflict",
                            method = "covariance")
 summary(cov_boot)
 to_latex(cov_boot)
 
 
 # sobel test with regression fit
-reg_sobel <- test_mediation(TeamCommitment ~
-                               m(TaskConflict) +
-                               ValueDiversity,
-                             data = BSG2014,
-                             test = "sobel")
+reg_sobel <- test_mediation(BSG2014,
+                            x = "ValueDiversity",
+                            y = "TeamCommitment",
+                            m = "TaskConflict",
+                            test = "sobel")
 summary(reg_sobel)
 to_latex(reg_sobel)
 
 
 # sobel test with coavariance fit
-cov_sobel <- test_mediation(TeamCommitment ~
-                              m(TaskConflict) +
-                              ValueDiversity,
-                            data = BSG2014,
+cov_sobel <- test_mediation(BSG2014,
+                            x = "ValueDiversity",
+                            y = "TeamCommitment",
+                            m = "TaskConflict",
                             test = "sobel",
                             method = "covariance")
 summary(cov_sobel)
