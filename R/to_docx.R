20b7cedb9941cadf482968e7b42ac096a7a22dae
Andreas Alfons <alfons@ese.eur.nl>
2023-03-01 17:09:33 +0100

    minor fix in to_docx()

--- R/to_docx.R
+++ R/to_docx.R
@@ -8,14 +8,14 @@
 #' @export
 to_docx <- function(object, ...) UseMethod("to_docx")
 
+#' @importFrom flextable body_add_flextable
 #' @importFrom officer read_docx
 #' @export
 to_docx.flextable <- function(object, file, ...) {
   # create .docx file
   docx <- officer::read_docx()
   # add flextables of results from mediation analysis
-  # FIXME: make sure the columns of the three tables are aligned
-  docx <- body_add_flextable(docx, object)
+  docx <- flextable::body_add_flextable(docx, object)
   # write to file
   print(docx, target = file)
   # return file invisibly
