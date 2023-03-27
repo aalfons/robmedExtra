5e6de0f0938e80b2f4d18750653af7624e71d526
Andreas Alfons <alfons@ese.eur.nl>
2023-03-27 19:11:25 +0200

    export_docx() now exports mediation tables in landscape mode to a page in landscape mode

--- R/export_docx.R
+++ R/export_docx.R
@@ -11,11 +11,22 @@ export_docx <- function(object, ...) UseMethod("export_docx")
 #' @importFrom flextable body_add_flextable
 #' @importFrom officer read_docx
 #' @export
+## TODO: play with font size and cell margins so that certain example tables
+##       fit nicely
 export_docx.flextable <- function(object, file, ...) {
   # create .docx file
   docx <- officer::read_docx()
   # add flextables of results from mediation analysis
   docx <- flextable::body_add_flextable(docx, object)
+  # if we have a mediation flextable in landscape mode, make sure that the
+  # Word document is in landscape mode as well
+  if (inherits(object, "mediation_flextable") &&
+      !is.null(object$orientation) &&
+      object$orientation == "landscape") {
+    docx <- body_end_section_landscape(docx)
+    # FIXME: we get a blank page in portrait mode at the end of the document
+    #        that should be removed
+  }
   # write to file
   print(docx, target = file)
   # return file invisibly
