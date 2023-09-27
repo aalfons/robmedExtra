75c92a7cb7b5c9bd4df7031b962b8abff9b21e2d
Andreas Alfons <alfons@ese.eur.nl>
2023-09-27 18:23:04 +0200

    function to create model diagram now implemented for one independent variable and parallel mediators

--- R/model_diagram.R
+++ R/model_diagram.R
@@ -11,7 +11,8 @@
 
 model_diagram <- function(x, y, m, covariates = NULL,
                           model = c("parallel", "serial"),
-                          width = 8, height = 2) {
+                          width = 8, height = 2,
+                          spacing = c(4, 1)) {
 
   # initializations
   p_x <- length(x)
@@ -26,6 +27,10 @@ model_diagram <- function(x, y, m, covariates = NULL,
   types_x <- rep.int(types, c(p_x, p_covariates))
   x <- c(x, covariates)
   p_x <- length(x)
+
+  # define useful sequences
+  if (p_x > 1L) seq_x <- seq_len(p_x)
+  if (p_m > 1L) seq_m <- seq_len(p_m)
 
   # define center of rectangles for independent variables
   if (p_x > 1L && p_m > 1L) {
@@ -44,7 +49,10 @@ model_diagram <- function(x, y, m, covariates = NULL,
 
     } else {
       # parallel multiple mediator model
-
+      center_x <- cbind(x = 0, y = 0)
+      center_m <- cbind(x = width + spacing[1L],
+                        y = height + (height + spacing[2L]) * (seq_m - 1))
+      center_y <- cbind(x = 2 * (width + spacing[1L]), y = 0)
     }
   } else if (p_x > 1L) {
     # multiple independent variables, one mediator
@@ -52,8 +60,8 @@ model_diagram <- function(x, y, m, covariates = NULL,
   } else {
     # simple mediation model
     center_x <- cbind(x = 0, y = 0)
-    center_m <- cbind(x = 10, y = 5)
-    center_y <- cbind(x = 20, y = 0)
+    center_m <- cbind(x = width + spacing[1L], y = 2 * height + spacing[2L])
+    center_y <- cbind(x = 2 * (width + spacing[1L]), y = 0)
   }
 
   # construct data frame for rectangles
@@ -80,7 +88,7 @@ model_diagram <- function(x, y, m, covariates = NULL,
                     ymin = df_boxes$y - height/2,
                     ymax = df_boxes$y + height/2)
 
-  # define data frames for arrows
+  # construct data frames for arrows
   if (p_x > 1L && p_m > 1L) {
     # multiple independent variables, multiple mediators
     if (have_serial) {
@@ -96,18 +104,42 @@ model_diagram <- function(x, y, m, covariates = NULL,
       # serial multiple mediator model
 
     } else {
-      # parallel multiple mediator model
-
+      ## parallel multiple mediator model
+      # find indices in data frame
+      which_x <- df_boxes$label == x
+      which_m <- df_boxes$label %in% m
+      which_y <- df_boxes$label == y
+      # construct data frame for arrows
+      step_xy <- height / (2 * p_m)
+      step_m <- height / (2 * (p_m - 1))
+      df_arrows <- rbind(
+        data.frame(x = df_boxes[which_x, "xmax"],
+                   y = df_boxes[which_x, "y"] + step_xy * seq_m,
+                   xend = df_boxes[which_m, "xmin"],
+                   yend = df_boxes[which_m, "y"] - step_m * (seq_m - 1),
+                   type = types_x),
+        data.frame(x = df_boxes[which_m, "xmax"],
+                   y = df_boxes[which_m, "y"] - step_m * (seq_m - 1),
+                   xend = df_boxes[which_y, "xmin"],
+                   yend = df_boxes[which_y, "y"] + step_xy * seq_m,
+                   type = types[1L]),
+        data.frame(x = df_boxes[which_x, "xmax"],
+                   y = df_boxes[which_x, "y"],
+                   xend = df_boxes[which_y, "xmin"],
+                   yend = df_boxes[which_y, "y"],
+                   type = types[1L])
+      )
     }
   } else if (p_x > 1L) {
     # multiple independent variables, one mediator
 
   } else {
+    ## simple mediation model
     # find indices in data frame
     which_x <- df_boxes$label == x
     which_m <- df_boxes$label == m
     which_y <- df_boxes$label == y
-    # simple mediation model
+    # construct data frame for arrows
     df_arrows <- rbind(
       data.frame(x = df_boxes[which_x, "xmax"],
                  y = df_boxes[which_x, "ymax"],
@@ -129,15 +161,15 @@ model_diagram <- function(x, y, m, covariates = NULL,
 
   # generate plot
   arrow_head <- arrow(angle = 15, length = unit(10, "pt"),
-                 ends = "last", type = "closed")
+                      ends = "last", type = "closed")
   ggplot() +
     geom_segment(mapping = aes(x = x, y = y, xend = xend, yend = yend,
                                color = type),
-                 data = df_arrows, size = 0.5, arrow = arrow_head,
+                 data = df_arrows, linewidth = 0.5, arrow = arrow_head,
                  show.legend = FALSE) +
     geom_rect(mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
                             color = type),
-              data = df_boxes, fill = "white", size = 0.5,
+              data = df_boxes, fill = "white", linewidth = 0.5,
               show.legend = FALSE) +
     geom_text(mapping = aes(x = x, y = y, label = label, color = type),
               data = df_boxes, show.legend = FALSE) +
