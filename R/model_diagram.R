5793b73c2644cad14c2151eb97092dc9a7b890ce
Andreas Alfons <alfons@ese.eur.nl>
2023-09-29 11:45:24 +0200

    proper handling of geoms in model_diagram()

--- R/model_diagram.R
+++ R/model_diagram.R
@@ -9,6 +9,7 @@
 #                         (only relevant if two or more mediators are supplied)
 # width, height ......... integers giving the width/height of the boxes
 #                         (the same for all boxes)
+#' @importFrom ggplot2 expansion
 #' @export
 setup_model_diagram <- function(x, y, m, covariates = NULL,
                                 model = c("parallel", "serial"),
@@ -39,7 +40,7 @@ setup_model_diagram <- function(x, y, m, covariates = NULL,
   }
   p_covariates <- length(covariates)
   # control variables will be indicated to be of different relevance
-  types <- c("primary", "secondary")
+  types <- c("regular", "control")
   # combine independent variables and control variables
   # (there is no difference in terms of the model)
   predictors <- c(x, covariates)
@@ -58,8 +59,6 @@ setup_model_diagram <- function(x, y, m, covariates = NULL,
   }
 
   # define useful sequences
-  seq_x <- seq_len(p_x)
-  seq_covariates <- seq_len(p_covariates)
   seq_predictors <- seq_len(p_predictors)
   seq_m <- seq_len(p_m)
 
@@ -229,6 +228,10 @@ setup_model_diagram <- function(x, y, m, covariates = NULL,
                stringsAsFactors = FALSE)
   )
 
+  # convert variable 'type' to a factor to ensure correct color allocation
+  df_boxes$type <- factor(df_boxes$type, levels = types)
+  df_arrows$type <- factor(df_arrows$type, levels = types)
+
   # order arrows so that the ones corresponding to control variables are drawn
   # first (then they are in the background relative to the other arrows)
   df_arrows <- df_arrows[order(df_arrows$type, decreasing = TRUE), ]
@@ -238,20 +241,18 @@ setup_model_diagram <- function(x, y, m, covariates = NULL,
   else text_size <- 4
 
   # return object
-  out <- list(boxes = df_boxes, arrows = df_arrows, text_size = text_size,
-              colors = c("black", "#808080"), expand_x = 0.1, expand_y = 0.1)
+  out <- list(boxes = df_boxes,
+              arrows = df_arrows,
+              colors = c("black", "#808080"),
+              size = text_size,
+              expand = expansion(add = 0.1))
   class(out) <- "setup_model_diagram"
   out
 
 }
 
 
-#' @importFrom grid arrow
-#' @importFrom ggplot2 coord_fixed expansion geom_rect geom_text geom_segment
-#' ggplot labs scale_color_manual scale_x_continuous scale_y_continuous
-#' theme_void
 #' @export
-
 model_diagram <- function(object, ...) UseMethod("model_diagram")
 
 #' @export
@@ -266,27 +267,115 @@ model_diagram.default <- function(object, x = object, y, m, covariates = NULL,
 
 }
 
+#' @importFrom ggplot2 coord_fixed expansion ggplot scale_color_manual
+#' scale_x_continuous scale_y_continuous theme_void
 #' @export
 model_diagram.setup_model_diagram <- function(object, ...) {
   # define style of arrow heads
-  arrow_head <- arrow(angle = 15, length = unit(8, "pt"),
-                      ends = "last", type = "closed")
   # generate plot
   ggplot() +
-    geom_segment(mapping = aes(x = x, y = y, xend = xend, yend = yend,
-                               color = type),
-                 data = object$arrows, linewidth = 0.5, arrow = arrow_head,
-                 show.legend = FALSE) +
-    geom_rect(mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
-                            color = type),
-              data = object$boxes, fill = "white", linewidth = 0.5,
-              show.legend = FALSE) +
-    geom_text(mapping = aes(x = x, y = y, label = label, color = type),
-              data = object$boxes, size = object$text_size,
-              show.legend = FALSE) +
+    geom_diagram_arrow(mapping = aes(x = x, y = y, xend = xend, yend = yend,
+                                     color = type),
+                       data = object$arrows, ...) +
+    geom_diagram_box(mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin,
+                                   ymax = ymax,color = type),
+                     data = object$boxes, ...) +
+    geom_diagram_label(mapping = aes(x = x, y = y, label = label, color = type),
+                       data = object$boxes, ..., default_size = object$size) +
     coord_fixed() +
     scale_color_manual("", values = object$colors) +
-    scale_x_continuous(expand = expansion(add = object$expand_x)) +
-    scale_y_continuous(expand = expansion(add = object$expand_y)) +
+    scale_x_continuous(expand = object$expand) +
+    scale_y_continuous(expand = object$expand) +
     theme_void()
+}
+
+
+## custom geom for arrows
+# - set suitable defaults for certain arguments: note that handling the line
+#   width properly is somewhat complicated since it can be set via different
+#   arguments ('linewidth" or 'lwd')
+# - always use the default stat = "identity" and position = "identity"
+# - avoid passing unknown arguments intended for other geoms
+#' @importFrom grid arrow
+#' @importFrom ggplot2 geom_segment
+geom_diagram_arrow <- function(...,
+                               # arguments with different default behavior
+                               arrow = NULL, show.legend = FALSE,
+                               # ignored arguments
+                               stat, position, angle, bg, family, fill,
+                               fontface, hjust, lineheight, size, vjust,
+                               parse, nudge_x, nudge_y, check_overlap) {
+  # the default arrow head needs to be set here rather than in the function
+  # definition, otherwise there is an error that has to do with lazy evaluation
+  # ("promise already under evaluation: recursive default argument reference")
+  if (is.null(arrow)) {
+    arrow <- arrow(angle = 15, length = unit(8, "pt"),
+                   ends = "last", type = "closed")
+  }
+  # extract argument names
+  arguments <- list(..., arrow = arrow, show.legend = show.legend)
+  argument_names <- names(arguments)
+  # replace argument names with standardized ones
+  standardized_names <- standardise_aes_names(argument_names)
+  names(arguments) <- standardized_names
+  # set default arguments if not specified otherwise
+  if (is.null(arguments$linewidth)) arguments$linewidth <- 0.5
+  # call existing geom function
+  do.call(geom_segment, arguments)
+}
+
+
+## custom geom for boxes
+# - set suitable defaults for certain arguments: note that handling the line
+#   width and fill color properly is somewhat complicated since they can be
+#   set via different arguments ('linewidth" or 'lwd', 'fill' or 'bg')
+# - always use the default stat = "identity" and position = "identity"
+# - avoid passing unknown arguments intended for other geoms
+#' @importFrom ggplot2 geom_rect
+geom_diagram_box <- function(...,
+                             # arguments with different default behavior
+                             show.legend = FALSE,
+                             # ignored arguments
+                             stat, position, angle, family, fontface, hjust,
+                             lineheight, size, vjust, arrow, arrow.fill,
+                             parse, nudge_x, nudge_y, check_overlap) {
+  # extract argument names
+  arguments <- list(..., show.legend = show.legend)
+  argument_names <- names(arguments)
+  # replace argument names with standardized ones
+  standardized_names <- standardise_aes_names(argument_names)
+  names(arguments) <- standardized_names
+  # set default arguments if not specified otherwise
+  if (is.null(arguments$fill)) arguments$fill <- "white"
+  if (is.null(arguments$linewidth)) arguments$linewidth <- 0.5
+  # call existing geom function
+  do.call(geom_rect, arguments)
+}
+
+
+## custom geom for label
+# - set suitable defaults for certain arguments: note that handling the text
+#   size properly is somewhat complicated since it can be set via different
+#   arguments ('size" or 'cex')
+# - always use the default stat = "identity" and position = "identity"
+# - avoid passing unknown arguments intended for other geoms
+#' @importFrom ggplot2 geom_text
+geom_diagram_label <- function(...,
+                               # argument to pass along the default size
+                               default_size,
+                               # arguments with different default behavior
+                               show.legend = FALSE,
+                               # ignored arguments
+                               stat, position, angle, bg, fill, linetype,
+                               linewidth, lty, lwd, arrow, arrow.fill) {
+  # extract argument names
+  arguments <- list(..., show.legend = show.legend)
+  argument_names <- names(arguments)
+  # replace argument names with standardized ones
+  standardized_names <- standardise_aes_names(argument_names)
+  names(arguments) <- standardized_names
+  # set default arguments if not specified otherwise
+  if (is.null(arguments$size)) arguments$size <- default_size
+  # call existing geom function
+  do.call(geom_text, arguments)
 }
